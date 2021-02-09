a4a5a4f8ea09c272c85334b3e7811d069baa66e8
ofac <ofaczero@gmail.com>
2021-02-08 21:02:58 -0500

    Category Seeder

--- larapp/database/seeders/DatabaseSeeder.php
+++ larapp/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
 
             UserSeeder::class,
-            //CategorySeeder::class,
+            CategorySeeder::class,
             //GameSeeder::class,
         ]);
     }
